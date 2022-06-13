007f43231078707759448b9c5386cea59ce8714f
Raid_fIVe <97727856+Inventive-Telecom-Partners@users.noreply.github.com>
2022-06-13 15:30:28 +0200

    2.0

--- Inventive_Inventory/resources/views/layout/admin.blade.php
+++ Inventive_Inventory/resources/views/layout/admin.blade.php
@@ -234,7 +234,7 @@
     <strong>Copyright &copy; 2022 Inventive Telecom Partner</strong>
     with <i class="fas fa-heart"></i>
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.5
+      <b>Version</b> 2.0
     </div>
   </footer>
 
--- Inventive_Inventory/resources/views/layout/home_layout.blade.php
+++ Inventive_Inventory/resources/views/layout/home_layout.blade.php
@@ -35,13 +35,13 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('admin/profil') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Mon Inventaire</a>
+                        <a href="{{ url('admin/profil') }}" class="btn btn-primary">Mon Inventaire</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Se connecter</a>
+                        <a href="{{ route('login') }}" class="btn btn-primary">Se connecter</a>
 
-                        @if (Route::has('register'))
+                        <!-- @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">S'enregistrer</a>
-                        @endif
+                        @endif -->
                     @endauth
                 </div>
             @endif
@@ -59,7 +59,7 @@
     <strong>Copyright &copy; 2022 Inventive Telecom Partner</strong>
     with <i class="fas fa-heart"></i>
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.5
+      <b>Version</b> 2.0
     </div>
   </footer>
 </div>
