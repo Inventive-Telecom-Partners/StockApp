33f2ae677462f39a22b873304d1a271e0dbca8a7
Raid_fIVe <97727856+Inventive-Telecom-Partners@users.noreply.github.com>
2022-06-15 02:41:36 +0200

    research done

--- Inventive_Inventory/app/Http/Controllers/AccueilController.php
+++ Inventive_Inventory/app/Http/Controllers/AccueilController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Stock;
+use App\Models\Shelf;
+use App\Models\Level;
 
 class AccueilController extends Controller
 {
@@ -58,7 +61,13 @@ class AccueilController extends Controller
     }
     
     public function research(){
-        $objets = DB::table("element")->get();
-        return view('research');
+        $changeLoca = DB::table("change_location")->get();
+        $elementData = DB::table("element")->get();
+        $categoryData = DB::table("category")->get();
+        $brandData = DB::table("brand")->get();
+        $stockData= Stock::all();
+        $shelfData = Shelf::all();
+        $levelData = Level::all();
+        return view('/research',['stockData'=>$stockData,'shelfData'=>$shelfData,'levelData'=>$levelData,'changeLoca'=>$changeLoca,'elementData'=>$elementData, 'categoryData'=>$categoryData,'brandData'=>$brandData]);
     }
 }
--- Inventive_Inventory/resources/views/research.blade.php
+++ Inventive_Inventory/resources/views/research.blade.php
@@ -24,17 +24,51 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>      
+          @foreach($stockData as $stock)
+            @foreach($shelfData as $shelf)
+              @if($stock->id == $shelf->idStock)
+                @foreach($levelData as $level)
+                  @if($shelf->id == $level->idShelf) 
+                    @foreach($changeLoca as $change)
+                      @if($level->id == $change->idLevel)
+                        @foreach($elementData as $element)
+                          @if($change->idElement == $element->id)
+                            @foreach($categoryData as $cat)
+                              @if($cat->id == $element->idCategory)
+                                @foreach($brandData as $brand)
+                                  @if($brand->id == $element->idBrand)
+                                    <tr>
+                                      <td>{{$stock->Stock_Name}} - {{$shelf->Shelf_Name}} - {{$level->Level_Name}}</td>
+                                      <td>{{$cat->Category_Name}}</td>
+                                      <td>{{$brand->Brand_Name}}</td>
+                                      <td>{{$element->Description}}</td>
+                                      <td>{{$element->Product_Number}}</td>
+                                      <td>{{$element->Serial_Number}}</td>
+                                      <td>{{$element->color}}</td>
+                                      <td></td>
+                                      <td>
+                                        <div class="row" style="margin-top:10px;">
+                                          <form action="{{ url ('/stockOut/'.$element->Serial_Number)}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <input type="submit" value="Sortir du stock" class="btn btn-danger">
+                                          </form>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  @endif
+                                @endforeach
+                              @endif
+                            @endforeach
+                          @endif
+                        @endforeach
+                      @endif
+                    @endforeach
+                  @endif 
+                @endforeach
+              @endif
+            @endforeach 
+          @endforeach     
         </tbody>
         <tfoot>
           <tr>
