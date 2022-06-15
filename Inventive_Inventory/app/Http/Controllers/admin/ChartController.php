e6508239c857beb005a77f78034f89d31d0774a9
Raid_fIVe <97727856+Inventive-Telecom-Partners@users.noreply.github.com>
2022-06-15 20:52:22 +0200

    Update ChartController.php

--- Inventive_Inventory/app/Http/Controllers/admin/ChartController.php
+++ Inventive_Inventory/app/Http/Controllers/admin/ChartController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Stock;
+use App\Models\Shelf;
+use App\Models\Level;
 
 class ChartController extends Controller
 {
@@ -12,6 +18,12 @@ class ChartController extends Controller
     }
 
     public function chartsAdmin(){
+        $changeLoca= DB::table('change_location')->get();
+        $countIn= $changeLoca->where('idFlow',1)->count();
+        $countOut= $changeLoca->where('idFlow',2)->count();
+        $stockData= Stock::all();
+        $shelfData = Shelf::all();
+        $levelData = Level::all();
         return view('admin/chartsAdmin');
     }
 
